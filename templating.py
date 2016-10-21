b80427edbe25e6559c352b22c640a96559cd4ead
bitfhacker <bitfhacker@gmail.com>
2016-10-21 02:16:54 +0100

    Update templating.py

--- templating.py
+++ templating.py
@@ -1,5 +1,4 @@
-rom string import Template
-#open the file
+from string import Template
 
 mytemplate = """$title
 
@@ -16,16 +15,13 @@ fffff
 $list
 """
 
-
-#filein = open( 'foo.txt' )
-#read it
-#src = Template( filein.read() )
 src = Template( mytemplate )
-#document data
+
 title = "This is the title"
 subtitle = "And this is the subtitle"
 list = ['first', 'second', 'third']
 d={ 'title':title, 'subtitle':subtitle, 'list':'\n'.join(list) }
-#do the substitution
+
 result = src.substitute(d)
+
 print result
