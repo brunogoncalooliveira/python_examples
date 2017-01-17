b21077777e7f59f8f221878ff5732dcf5faa8c92
bitfhacker <bitfhacker@gmail.com>
2017-01-17 14:13:38 +0000

    Update excel.py

--- excel.py
+++ excel.py
@@ -29,4 +29,4 @@ def get_Excel_as_dict(file, getDictBy=None, fields=[]):
                     arr[row[header[ chave ]].value][i] = row[header[ i ]].value
     return arr
 
-pprint(get_modulo_equipa('example.xlsx', getDictBy='MyKeyField', fields=['Field1', u'Field2' ]))
+pprint(get_Excel_as_dict('example.xlsx', getDictBy='MyKeyField', fields=['Field1', u'Field2' ]))
