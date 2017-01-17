6d00fa48dcc9c152fba934879b9e07d129ccfe14
bitfhacker <bitfhacker@gmail.com>
2017-01-17 14:12:25 +0000

    Example of reading an excel with python
    This examples returns a dict with fields indicated in "fields"

---
+++ excel.py
@@ -0,0 +1,31 @@
+from openpyxl import load_workbook
+
+def get_Excel_as_dict(file, getDictBy=None, fields=[]):
+
+    wb2 = load_workbook(file)
+    sheet = wb2['Sheet1']
+
+    first = True
+
+    if getDictBy == None:
+        raise Exception('O campo getDictBy tem que ter um valor que corresponde a um nome de coluna no ficheiro excel')
+
+    header = {}
+
+    chave = getDictBy
+    arr = {}
+
+    for row in sheet.rows:
+        if first:
+            for i in row:
+                header[ i.value ] = i.col_idx - 1
+            first = False
+        else:
+            if row[header[ chave ]].value != '':
+                for i in fields:
+                    if row[header[ chave ]].value not in arr:
+                        arr[row[header[ chave ]].value] = {}
+                    arr[row[header[ chave ]].value][i] = row[header[ i ]].value
+    return arr
+
+pprint(get_modulo_equipa('example.xlsx', getDictBy='MyKeyField', fields=['Field1', u'Field2' ]))
