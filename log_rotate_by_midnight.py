ce3ab62201c4f814638b3f7534f67fc4a03e439b
bitfhacker <bitfhacker@gmail.com>
2017-11-13 11:35:35 +0000

    Update log_rotate_by_midnight.py

--- log_rotate_by_midnight.py
+++ log_rotate_by_midnight.py
@@ -1,18 +1,16 @@
 import logging
 from logging import handlers
 
-LOG_FILENAME = 'mylog.txt'
-
-def log_rotate_by_midnight(path):
+def log_rotate_by_midnight(path, namespace):
     logformatter = logging.Formatter('%(asctime)s;%(levelname)s;%(message)s')
-    log = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, 'midnight', 1, backupCount=5)
+    log = logging.handlers.TimedRotatingFileHandler(path, 'midnight', 1, backupCount=5)
     log.setLevel(logging.DEBUG)
     log.setFormatter(logformatter)
 
-    tmp = logging.getLogger('main')
+    tmp = logging.getLogger(namespace)
     tmp.addHandler(log)
     tmp.setLevel(logging.DEBUG)
     return tmp
 
-logger = log_rotate_by_midnight(LOG_FILENAME)
-logger.error('writting this message to log')
+logger1 = log_rotate_by_midnight('invalidapps.log', 'invalidapps_namespace')
+logger2 = log_rotate_by_midnight('other.log', 'otherinvalidsomething_namespace')
