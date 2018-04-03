2ab656857820e0bcc0cdac66b3fd1a99716cc03e
bitfhacker <bitfhacker@gmail.com>
2018-04-03 14:26:39 +0100

    Create logging.py

---
+++ logging.py
@@ -0,0 +1,7 @@
+import logging
+
+LOG_FILENAME = 'debug.log'
+logging.basicConfig(filename=LOG_FILENAME,
+                    level=logging.INFO,
+                    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S")
